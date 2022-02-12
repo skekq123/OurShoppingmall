84495804ad5e6f2cf3cc97045b642e08c8e08103
rlaalsrl715 <minkik715@gmail.com>
2022-02-12 16:19:50 +0900

    테스트 완료

--- src/main/java/com/sparta/ourshoppingmall/OurshoppingmallApplication.java
+++ src/main/java/com/sparta/ourshoppingmall/OurshoppingmallApplication.java
@@ -2,13 +2,9 @@ package com.sparta.ourshoppingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-=======
-
->>>>>>> 03d8832973aafb9462a39935e1d8372bce90ffdb
 @SpringBootApplication
 public class OurshoppingmallApplication {
 
